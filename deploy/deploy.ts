f0d5c788611201d348b97f22e1ad78c7495fa9a8
sammywub <tbygmhcuq595@outlook.com>
2025-11-28 11:16:32 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedZSphereGame = await deploy("ZSphereGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZSphereGame contract: `, deployedZSphereGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zsphere_game"; // id required to prevent reexecution
+func.tags = ["ZSphereGame"];
